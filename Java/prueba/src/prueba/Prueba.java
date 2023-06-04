34afcd794a6995bc8739ab9eed63758d3da9e94b
Miguel Flores <m.ang.flores86@gmail.com>
2023-06-04 20:07:28 -0300

    create-project/testing-Git

---
+++ Java/prueba/src/prueba/Prueba.java
@@ -0,0 +1,24 @@
+/*
+ Esto es una prueba para aprender GIT
+ */
+package prueba;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Prueba {
+
+    /**
+       */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Prueba exitosa?");
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+        String Respuesta = leer.next();
+        System.out.println(Respuesta);
+    }
+    
+}
